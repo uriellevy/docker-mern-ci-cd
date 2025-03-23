f6085ee412010d5300ac85f6369798080f496662
uriellevy <uriellevy4892@gmail.com>
2025-03-23 16:06:15 +0200

    deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,13 +22,8 @@ const allowedOrigins = [
 ];
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
+
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
